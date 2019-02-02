bbe8d724afb85d217f63246d8966254cbb0ba75a
Violeta Moloney <Violeta.Moloney@mail.itsligo.ie>
2019-02-02 15:50:33 +0000

    later changes

---
+++ Week1App/pages/greeting.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Week1App.pages
+{
+    public class greetingModel : PageModel
+    {
+        public void OnGet()
+        {
+            if (DateTime.Now.Hour >= 12)
+            {
+                Message = "Good Afternoon";
+            }
+            else {
+                Message = "Good Morning";
+            }
+
+        }
+
+        public string Message { get; set; } = "Hello from razor page";
+    }
+}
